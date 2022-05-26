e161ddb7061b38f4507b2b09d006540628a6a962
Istvan Tanko <tistvan1989@gmail.com>
2022-05-26 18:53:30 +0300

    Section 9: Lists, Teleport, Template Refs & nextTick

---
+++ src/views/ModalsView.vue
@@ -0,0 +1,43 @@
+<template>
+  	<div class="modals">
+    	<h1>Modals</h1>
+		<button @click="showModal = true">Show modal</button>
+		<teleport to=".modals-container">
+			<div
+			v-show="showModal"
+			class="modal"
+			>
+				<h1>This is a modal</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia excepturi doloribus et beatae consequatur voluptates itaque ad recusandae voluptatum quasi. Maiores modi consequatur repellat velit quidem dicta doloribus! Maiores, quia?</p>
+				<button @click="showModal = false">Hide modal</button>
+			</div>
+		</teleport>
+  	</div>
+</template>
+
+<script setup>
+
+/*
+	import
+*/
+import { ref } from 'vue'
+
+/*
+	modals
+*/
+const showModal = ref(false)
+
+</script>
+
+<style>
+.modal {
+	background-color: beige;
+	padding: 10px;
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	z-index: 1;
+}
+</style>
